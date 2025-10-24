51c2efc10e6bf36203013ab52b16885ee430c08e
ThomasCCC <130942400+zchiang77@users.noreply.github.com>
2025-10-24 15:21:48 +0800

    Add files via upload

---
+++ example/domain.py
@@ -0,0 +1,77 @@
+"""
+This is an example of TADs calling using the individual approach.
+Run example/run1.py before running this script.
+"""
+
+import get_domain_helper
+import cooler
+import os
+import time
+import pandas as pd
+import numpy as np
+from itertools import combinations
+
+pair_merge = False
+parent_dir = "PATH/TO/example/"
+exp_list = ['rep1']
+chrom_list = ['2']
+score_threshold = 0
+utest_significance = 0
+resolution = 5000
+domain_limit = 50000
+max_iteration = 0
+scale_dist = [4]
+
+if pair_merge:
+    os.system("mkdir " + parent_dir + "topohic/merge/")
+    exp_combination = list(combinations(exp_list, 2))
+    print("==================  CURRENT PROCESS  ==================")
+    for exp_comb in exp_combination:
+        start_time = time.time()
+        exp1 = exp_comb[0]
+        exp2 = exp_comb[1]
+        exp_example = cooler.Cooler(parent_dir + exp1 + "-5kb.cool")
+        for chrom in chrom_list:
+            cooler_ext = exp_example.extent(chrom)
+            bin_range = list(cooler_ext)
+            bin_range[1] -= 1
+            for scd in scale_dist:
+                directory1 = (parent_dir + "topohic/" + exp1 + "/topohic-" + chrom + "/" +
+                              exp1 + "-sc" + str(scd) + "/score.csv")
+                directory2 = (parent_dir + "topohic/" + exp2 + "/topohic-" + chrom + "/" +
+                              exp2 + "-sc" + str(scd) + "/score.csv")
+                score1 = pd.read_csv(directory1)
+                score2 = pd.read_csv(directory2)
+                y1 = np.array(score1['loop_enrichment'])
+                y2 = np.array(score2['loop_enrichment'])
+                score = pd.DataFrame({'bin_id': list(score1['bin_id']),
+                                      'loop_enrichment': np.mean([y1, y2], axis=0).tolist()})
+                directory_merge = parent_dir + "topohic/merge/" + exp1 + "_" + exp2 + "_" + chrom + "_sc" + str(scd)
+                score.to_csv(directory_merge + "_score.csv", header=True, index=False)
+                get_domain_helper.get_domain(directory_merge, score_threshold, bin_range, utest_significance,
+                                             max_iteration, resolution, domain_limit, pair_merge)
+        end_time = time.time()
+        print("Finding TADs for {}&{} takes {} seconds.".format(exp1, exp2, end_time - start_time))
+    print("=======================================================\n")
+else:
+    print("==================  CURRENT PROCESS  ==================")
+    for exp_name in exp_list:
+        start_time = time.time()
+        exp_cooler = cooler.Cooler(parent_dir + exp_name + "-5kb.cool")
+        for chrom in chrom_list:
+            cooler_ext = exp_cooler.extent(chrom)
+            bin_range = list(cooler_ext)
+            bin_range[1] -= 1
+            for scd in scale_dist:
+                directory = (parent_dir + "topohic/" + exp_name + "/topohic-" + chrom + "/" +
+                             exp_name + "-sc" + str(scd))
+                get_domain_helper.get_domain(directory, score_threshold, bin_range, utest_significance,
+                                             max_iteration, resolution, domain_limit, pair_merge)
+        end_time = time.time()
+        print("Finding TADs for {} takes {} seconds.".format(exp_name, end_time - start_time))
+    print("=======================================================\n")
+
+
+
+
+
---
+++ example/run1.py
@@ -0,0 +1,79 @@
+"""
+This is an example that processes rep1-5kb.cool and computes loop scores.
+Before running this script, you need to download the .hic file from GSM1551568 and convert it into .cool format.
+"""
+
+import find_thresh
+import hic_edges
+import compute_PH
+import get_scale
+import get_score
+import cooler
+import os
+import time
+
+parent_dir = "PATH/TO/example/"
+exp_list = ['rep1']
+weight_col = "weight"
+chrom_list = ['2']
+resolution = 5000
+edge_limit_bp = -1
+
+scale_dist = [4]
+compute_end_perc = "25%"
+compute_end_perc_dist = 50
+scale_perc = "75%"
+significance = 0
+
+filetype = 2  # This indicates that the source file is in (vertex, vertex, edge) form
+dim = 1  # PH computation up to dim
+begin_thresh = 0
+threads = 64
+
+os.system("mkdir " + parent_dir + "topohic/")
+
+for exp_name in exp_list:
+
+    start_time = time.time()
+    os.system("mkdir " + parent_dir + "topohic/" + exp_name)
+
+    for chrom in chrom_list:
+
+        cool_file = parent_dir + exp_name + "-5kb.cool"
+        output_path = parent_dir + "topohic/" + exp_name + "/topohic-" + chrom + "/"
+
+        cooler_obj = cooler.Cooler(cool_file)
+        cooler_ext = cooler_obj.extent(chrom)
+        bin_range = list(cooler_ext)
+        bin_range[1] -= 1
+        print("NOTE: USING BINS RANGING FROM {} TO {} !!".format(bin_range[0], bin_range[1]))
+
+        os.system("mkdir " + output_path)
+
+        if edge_limit_bp == -1:
+            edge_limit = -1
+        else:
+            edge_limit = edge_limit_bp / resolution
+        find_thresh_output = find_thresh.find_thresh(cool_file, exp_name, weight_col, bin_range[0], bin_range[1],
+                                                     output_path, edge_limit)
+        edge_output, end_thresh = hic_edges.hic_edges(cool_file, exp_name, weight_col, find_thresh_output,
+                                                      bin_range[0], bin_range[1], output_path,
+                                                      compute_end_perc, compute_end_perc_dist, edge_limit)
+
+        for scd in scale_dist:
+            sc = get_scale.get_scale(find_thresh_output, scale_perc, scd)
+            directory_for_ph = output_path + exp_name + "-sc" + str(scd) + "/"
+            os.system("mkdir " + directory_for_ph)
+            cyc_file = compute_PH.compute_ph(edge_output, exp_name, directory_for_ph, filetype,
+                                             dim, begin_thresh, threads, end_thresh, sc)
+            get_score.get_score(exp_name, directory_for_ph, bin_range, resolution)
+
+    end_time = time.time()
+
+    print("==================  CURRENT PROCESS  ==================")
+    print("topohic for {} takes {} seconds.".format(exp_name, end_time - start_time))
+    print("=======================================================\n")
+
+
+
+
